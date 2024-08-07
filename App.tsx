e39175ef7d47c9eb3793ecd231d2b39af1cda21a
Muhammad Asif <145665646+muhammadasifdotai@users.noreply.github.com>
2024-08-07 18:42:51 +0500

    testing branch one

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App(): JSX.Element {
   return (
-    <View>
-      <Text>Alhamdulillah</Text>
+    <View style={styles.main}>
+      <Text style={styles.text}>Alhamdulillah</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  main: {
+    backgroundColor: 'red',
+  },
+  text: {
+    fontSize: 40,
+  }
+})
